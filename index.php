43507cb15e7437085959369f1c4ed87524de52b9
reformedhope <steph_salzburg@hotmail.com>
2023-11-11 04:18:56 +0000

    learned about keywords/functions

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
     <?php
 
 
-echo "<h1>hello world!</h1>";
-echo "<h1>my name is Stephanie</h1>";
+// echo "<h1>hello world!</h1>";
+// echo "<h1>my name is Stephanie</h1>";
+
+
+$age = 28;
+
+$data = null;
+$isLoggedIn = true;
+$hasBlogPosts;
+var_dump($isLoggedIn);
+
+
 
 ?> 
 
