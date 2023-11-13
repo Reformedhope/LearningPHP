443b49a4da030649ca46a716fafbe9de8d3e2dbd
reformedhope <steph_salzburg@hotmail.com>
2023-11-13 01:44:48 +0000

    clearned type casting

--- index.php
+++ index.php
@@ -2,17 +2,9 @@
     <?php
 
 
-// echo "<h1>hello world!</h1>";
-// echo "<h1>my name is Stephanie</h1>";
 
 
-$data = ['kelly' => 'Salad', 'Sally' => 'hotdog', 'jane' => 'sandwich'];
 
-$data['tom'] = 'chicken';
-
-$data['bob'] = 'tomato soup';
-
-var_dump($data);
 
 
 
