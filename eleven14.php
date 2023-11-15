edfdced0646602edfe6ac88dee8c7115f5769940
reformedhope <steph_salzburg@hotmail.com>
2023-11-15 02:57:33 +0000

    updated files

--- eleven14.php
+++ eleven14.php
@@ -1,35 +1,58 @@
+ <?php
+ var_dump(
+   1 != 1,
+    1 !== "1",   
+      2 < 3,
+     2 > 3,    
+      2 <= 3,
+     2 >= 3,
+ ); 
+  ?> 
 
-<!-- <?php
+<!-- // ------- -->
 
-var_dump(
-    1 != 1,
-    1 !== "1",
-    2 < 3,
-    2 > 3,
-    2 <= 3,
-    2 >= 3,
-); ?>
-
-// -------
-
-
-
-// <?php
-// // error control opperator
-// @var_dump(
-//    (string)[1]
-// );
+ <?php
+ // error control opperator
+ @var_dump(
+    (string)[1]
+);
+?>
 
 
 // --------
 
-// <?php
-// $age = 28;
-// $permission = "";
-// $data = !$permission;
+ <?php
+ $age = 28;
+ $permission = "";
+ $data = !$permission;
+ var_dump($data)
+ ?> 
+
+ // --------
+ <?php
+$age = 28;
+$permission = "";
+$data = !$permission;
+var_dump($data)
+
+?> 
 
 
-// var_dump($data)
+//-------
+
+<?php
+$fullName = "john Smith";
+$fullName = 100;
+
+const FULL_NAME = "John Smith ";
+const FULL_NAME = "Jane";
+
+var_dump(FULL_NAME);
+
+
+?>
+
+//------
 
 
 
@@ -37,14 +60,16 @@ var_dump(
 
 
 
-// ?> 
 
 
 
 
 
 
-// ?> 
+
+
+
+
 
 
 
--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 
     <?php
-$fullName = "john Smith";
-$fullName = 100;
 
-const FULL_NAME = "John Smith ";
-const FULL_NAME = "Jane";
-
-var_dump(FULL_NAME);
 
 
 ?> 
