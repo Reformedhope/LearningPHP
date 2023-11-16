357b63b99578c2c16157909e6643f513265efd28
reformedhope <steph_salzburg@hotmail.com>
2023-11-16 02:28:43 +0000

    control structures

---
+++ Eleven15.php
@@ -0,0 +1,17 @@
+<?php
+    $fullName = 'John Smith';
+    $fullName = 100;
+    const FULL_NAME = "John Smith";
+    $message = "Hello, my name is ";
+    $message .= FULL_NAME;
+
+    var_dump($message. FULL_NAME);
+
+
+?> 
+
+
+//----------
+
+
+  
--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 
     <?php
+   
 
+   $score = 100;
+
+   if ($score >  90) {
+    var_dump("winner");
+   }elseif ($score > 80){
+    var_dump("loser");
+    }elseif ($score > 0){
+    var_dump("MEAT");
+   }else{
+    var_dump("paw");
+   }
 
 
 ?> 
