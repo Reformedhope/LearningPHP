dda16339f2ef829d26a13a79afb461b41916d311
reformedhope <steph_salzburg@hotmail.com>
2023-11-21 00:42:23 +0000

    varidic Arguments

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php require_once 'nav.php'; ?>
 
+<?php
+
+function sum(bool $dumpArr, int|float ...$nums) {
+    if ($dumpArr){
+    var_dump($nums);
+    }
+    return array_sum($nums);
+}
+
+echo sum(5,2,9,1);
+
+?>
+
